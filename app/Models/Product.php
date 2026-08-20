749e2fff67cb1d9a46384c94754aeca6d06e8e6d
Nasta Emanuele <{your_email}>
2026-08-20 20:46:22 +0200

    aggiunte le relazioni Eloquent per prodotti, ordini e righe ordine

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model {
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price_cents',
+        'stock',
+    ];
+    // usefull if in the future we want to see what order have bought this product
+    public function orderItems(): HasMany {
+        return $this->hasMany(OrderItem::class);
+    }
+    // it return the price in euros instead of cents
+    public function getPriceAttribute(): float {
+        return $this->price_cents / 100;
+    }
+}
